97a162938720a8ff7b23559bce3e93b383bd6c22
Benas Petrauskas <arijusgrotuzas@gmail.com>
2022-01-05 17:53:12 +0100

    Added image compression from csv files

--- main.cpp
+++ main.cpp
@@ -53,7 +53,6 @@ int KNearestNeighbourClass(vector<point> arr, int classNum, int k, point p){
     // Calculate the distance from a given point and each point in the dataset
     for (int i = 0; i < arr.size(); i ++)
         arr[i].distance = EuclideanDistance(arr[i].features, p.features);
-        //arr[i].distance = sqrt((arr[i].x - p.x) * (arr[i].x - p.x) + (arr[i].y - p.y) * (arr[i].y - p.y));
 
     // Sort points array based on the distance
     sort(arr.begin(), arr.end(), compareDistance);
